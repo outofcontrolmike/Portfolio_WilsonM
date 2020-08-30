1dd1941718b114ee0be1f0507abf15869d8f7581
MAW_CIS_Student <mw0415436@otc.edu>
2020-08-29 20:09:11 -0500

    Added projects into Portfolio for Github

---
+++ ContactForm/Entertianment Website - Javascript/main.js
@@ -0,0 +1,9 @@
+// Function that stretches the header accross screen
+$(document).ready(function(){
+    $('.header').height($(window).height());
+  })
+
+
+  //Home page seciton ---------------------------------------------//
+
+  //On click event for images
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Portfolio_WilsonM
 My Code Portfolio
+
+These are projects from my time in school studying CIS as well as life after graduation.
+
+My main goal here is to list only finished projects in this Portfolio.
